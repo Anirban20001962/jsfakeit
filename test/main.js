d17ff867ce5bab90c4badb491ee3c3778a357dbe
Anirban20001962 <anirbandhakuria@gmail.com>
2022-01-13 12:02:49 +0530

    some fixes

--- test/main.js
+++
@@ -1,9 +0,0 @@
-const jsfakeit = require('../dist/index');
-
-console.log(jsfakeit.address());
-console.log(jsfakeit.city());
-console.log(jsfakeit.country());
-console.log(jsfakeit.state());
-console.log(jsfakeit.street());
-console.log(jsfakeit.longitude());
-console.log(jsfakeit.latitude());

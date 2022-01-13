7c8487f9ecfaeb8f2c17aa20120dea179bb96ceb
Anirban20001962 <anirbandhakuria@gmail.com>
2022-01-13 20:27:59 +0530

    beer update

--- src/beer/index.ts
+++ src/beer/index.ts
@@ -1,6 +1,6 @@
 import { beer as beerData } from '../data/beer';
 
-import { chooseRand } from '../helper/chooseRand';
+import { chooseRand, getRandomArbitrary } from '../helper/chooseRand';
 
 // Generates random beer name
 export const beerName = () => {
@@ -18,7 +18,19 @@ export const beerHop = () => {
 export const beerMalt = () => {
 	return chooseRand<typeof beerData>('malt', beerData);
 };
-
+// Generates random beer yeast
 export const beerYeast = () => {
 	return chooseRand<typeof beerData>('yeast', beerData);
+};
+// BeerAlcohol will return a random beer alcohol level between 2.0 and 10.0
+export const beerAlcohol = () => {
+	return getRandomArbitrary(2, 10).toFixed(1);
+};
+// BeerIbu will return a random beer ibu value between 10 and 100
+export const beerIbu = () => {
+	return getRandomArbitrary(10, 100).toFixed(0);
+};
+// BeerBlg will return a random beer blg between 5.0 and 20.0
+export const beerBlg = () => {
+	return getRandomArbitrary(5, 20).toFixed(1);
 };
